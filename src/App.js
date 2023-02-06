76cbea170356ede2c8916eb8a09acf7dbd60670d
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-02-05 22:48:59 -0600

    added gh pages so i can deploy project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
+import Headphones from './components/Headphones';
 function App() {
   return (
     <>
---
+++ src/components/Headphones.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Headphones() {
+  return (
+    <>
+      <div className="headphone-nav">
+        <div className="nav">
+          <h1>audiophile</h1>
+
+          <div class="menu">
+            <a href=" ">HOME</a>
+            <a href="">HEADPHONES</a>
+            <a href=" ">SPEAKERS</a>
+            <a href="">EARPHONES</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
